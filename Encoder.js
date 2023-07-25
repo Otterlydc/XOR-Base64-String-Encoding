4839d9300901e4f202b2f874b035707e03e445f1
Otterlydc <127922057+Otterlydc@users.noreply.github.com>
2023-07-25 18:12:28 +1200

    Create Encoder.js
    A Simple String Encoding JavaScript Class

---
+++ Encoder.js
@@ -0,0 +1,21 @@
+/* MessageEncoder */
+class MessageEncoder {
+    constructor() {
+        this.shiftedChars = [];
+    };
+    Encode(string) {
+        try {
+            const encodedString = btoa(string);
+            this.shiftedChars.length = 0;
+            for (let i = 0; i < encodedString.length; i++) {
+                const shiftedCharCode = (encodedString.charCodeAt(i) - 42069) % 256;
+                this.shiftedChars.push(shiftedCharCode);
+            }
+            const shiftedString = String.fromCharCode(...this.shiftedChars);
+            return shiftedString;
+        } catch (error) {
+            console.error("Error Encoding", error);
+        };
+    };
+};
+let encoder = new MessageEncoder();
